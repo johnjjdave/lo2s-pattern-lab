97f807c9ae84e7147d1c5030ec53c333aeb488d5
John Dave <50245251+johnjjdave@users.noreply.github.com>
2026-07-31 21:09:59 +0400

    Add desktop interface entry

---
+++ desktop/src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "../../app/page";
+import "../../app/globals.css";
+import "./desktop.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+);
+
